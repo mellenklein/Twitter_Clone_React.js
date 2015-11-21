173f3359b69012520cf482a49e3a6cf35d63f7e7
Mary Ellen Klein <maryellenklein@gmail.com>
2015-11-21 17:32:40 -0600

    adding user page content and styling

---
+++ src/js/user.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+import jQuery from 'jquery';
+import _ from 'lodash';
+var moment = require('moment');
+moment().format();
+
+class User extends React.Component {
+  render() {
+    let date = this.props.user.created_at;
+    let momentTime = new Date(date).toDateString();
+    return (
+      <article className="user">
+        <img className="coverPic" src="http://lorempixel.com/300/100/abstract"/>
+        <img className="avatar" src="http://lorempixel.com/50/50/people"/>
+        <a className="followBtn">Follow</a>
+        <a href="#" className="fa fa-cog"></a>
+        <header className="userHeader">
+          <p className="name">Firstname Lastname</p>
+          <a className="email" href="#/users/:userId">{this.props.user.email}</a>
+          <time>Joined {momentTime}</time>
+          <p className="bio">This is my bio.</p>
+          <a href="#" className="fa fa-user-plus"></a>
+        </header>
+      </article>
+    )
+  }
+}
+
+export default User;
